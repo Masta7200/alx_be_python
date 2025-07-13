95f3e4cf90c9c1ffef0ba649e5559b10ff49d985
Masta7200 <131053603+Masta7200@users.noreply.github.com>
2025-07-13 05:35:40 -0800

    Create future_age_calculator.py

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,4 @@
+age = int(input("Enter your current age: "))
+years_to_add = int(input("How many years do you want to add? "))
+future_age = age + years_to_add
+print(f"In {2025} , you will be {future_age} years old.")
