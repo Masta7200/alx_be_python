4094e847e30b768d15a95626b465902574d8952b
Masta7200 <131053603+Masta7200@users.noreply.github.com>
2025-07-13 05:04:47 -0800

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,1 +1,8 @@
-
+number1=10
+number2=5
+sum = number1 + number2
+diference = number1 - number2
+product = number1 * number2
+print("Addition of 10 and 5 is:", sum)
+print("Subtraction of 10 and 5 is:", diference)
+print("Multiplcation of 10 and 5 is:", product)
