598f1423aade3c864ccfca6af6b2f29026fb4469
moustapha7200@gmail.com <moustapha7200@gmail.com>
2025-07-20 13:39:07 -0800

    new commit

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,4 +1,4 @@
-current_weather= input("What's the weather like today? (sunny/rainy/cold): ").string().lower()
+current_weather= input("What's the weather like today? (sunny/rainy/cold): ")
 if current_weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
 elif current_weather == "rainy":
